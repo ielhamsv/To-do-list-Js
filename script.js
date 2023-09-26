85125f6582941ace993eb12e320f74c7f8643c04
Elham Mousavi <elham.msv531@gmail.com>
2023-09-26 06:54:46 -0700

    remove item from list

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const item_form = document.getElementById('item-form');
 const item_input=document.getElementById('item-input')
 const input_invalid=document.getElementById('input-invalid')
 const item_list=document.getElementById("item-list")
+const clear_btn=document.getElementById('items-clear')
+const itemFilter=document.getElementById('filter')
+
+
+
+function displayItems(){
+    let itemsFromStorage
+
+    if (localStorage.getItem('items')!==null){
+        itemsFromStorage=JSON.parse(localStorage.getItem('items'))
+    }else{
+        itemsFromStorage=[]
+    }
+
+    itemsFromStorage.forEach((item => addItemToDom(item)))
+    checkUI()
+}
+
 
 function addItem(e){
     e.preventDefault()
@@ -15,9 +33,20 @@ function addItem(e){
         input_invalid.innerText=''
     }
 
+    addItemToDom(newItem)
+
+    addItemToStorage(newItem)
+
+   item_input.value=''
+
+    checkUI()
+}
+
+
+function addItemToDom(item){
     const li = document.createElement('li')
     li.className='list-item'
-    li.textContent=newItem
+    li.textContent=item
 
     const icon=creatIcon('ri-close-circle-line')
 
@@ -25,7 +54,19 @@ function addItem(e){
 
     item_list.appendChild(li)
 
-   item_input.value=''
+}
+
+function addItemToStorage (item){
+    let itemsFromStorage
+
+    if (localStorage.getItem('items')!==null){
+        itemsFromStorage=JSON.parse(localStorage.getItem('items'))
+    }else{
+        itemsFromStorage=[]
+    }
+
+    itemsFromStorage.push(item)
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
 }
 
 function creatIcon(classes){
@@ -36,5 +77,64 @@ function creatIcon(classes){
 }
 
 
+function onClickItem(e){
+    if (e.target.classList.contains('ri-close-circle-line')){
+        e.target.parentElement.remove()
+    }
+    removeItemFromStorage(e.target.parentElement.textContent)
+    checkUI()
+}
+
+function clearItems(){
+    item_list.innerHTML=''
+    localStorage.removeItem('items')
+    checkUI()
+}
+
+function removeItemFromStorage(item){
+    let itemsFromStorage
+
+    if (localStorage.getItem('items')!==null){
+        itemsFromStorage=JSON.parse(localStorage.getItem('items'))
+    }else{
+        itemsFromStorage=[]
+    }
+    itemsFromStorage=itemsFromStorage.filter((i) => i !== item)
+
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+}
+
+
+function filterItems(e) {
+    const items = item_list.querySelectorAll('li')
+    const text = e.target.value
+
+    items.forEach((item) => {
+      const itemName=item.firstChild.textContent
+        if (itemName.indexOf(text) !== -1){
+            item.style.display='flex'
+        }else {
+            item.style.display='none'
+        }
+    })
+}
+function checkUI(){
+    const items = item_list.querySelectorAll('li')
+
+    if (items.length===0){
+        clear_btn.style.display='none'
+        itemFilter.style.display='none'
+    }else {
+        clear_btn.style.display='block'
+        itemFilter.style.display='block'
+    }
+}
+
 // Event Listener
 item_form.addEventListener("submit", addItem)
+item_list.addEventListener('click', onClickItem)
+clear_btn.addEventListener('click',clearItems)
+itemFilter.addEventListener('input', filterItems)
+document.addEventListener('DOMContentLoaded', displayItems)
+
+checkUI()
